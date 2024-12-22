4914682e47e56a7954bc740b843ee90f723c382e
Wildan Miladji <wimmmy58@gmail.com>
2024-12-23 06:49:43 +0700

    main app

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+# Menetapkan konfigurasi halaman agar tidak ada sidebar
+st.set_page_config(page_title="ALus", layout="wide", initial_sidebar_state="collapsed")
+
+from home import display_home
+from diagnose import display_diagnosis
+from about import display_about
+from chatbot import display_chatbot
+
+# Navbar dengan radio button di bagian atas
+menu = ["Beranda", "Konsultasi", "Diagnosa", "Tentang"]
+choice = st.radio("Navigasi", menu, horizontal=True)
+
+# Tampilkan halaman sesuai pilihan dari radio button
+if choice == "Beranda":
+    display_home()
+
+elif choice == "Konsultasi":
+    display_chatbot()
+
+elif choice == "Diagnosa":
+    display_diagnosis()
+
+elif choice == "Tentang":
+    display_about()
